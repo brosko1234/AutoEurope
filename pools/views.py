d425f1be4f8c4d124d73a29480c03af31597bfa2
brosko1234 <brosko.ev@yandex.ru>
2022-11-05 15:52:28 +0300

    add

---
+++ pools/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.file.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
